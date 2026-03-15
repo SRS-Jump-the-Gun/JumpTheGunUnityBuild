df94c878c34f96fad32ce6f4028e465cee44a22c
zachsea <zachse4@gmail.com>
2026-03-15 02:26:11 -0700

    feat: Add text dependency for displaying player health

--- JumpTheGun/Assets/Scripts/PlayerHealth.cs
+++ JumpTheGun/Assets/Scripts/PlayerHealth.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour, IDamageable
@@ -5,10 +6,12 @@ public class PlayerHealth : MonoBehaviour, IDamageable
     [Header("Health")]
     public int maxHP = 100;
     private int currentHP;
+    [SerializeField] protected TMP_Text hpText;
 
     private void Awake()
     {
         currentHP = maxHP;
+        hpText.text = currentHP.ToString();
     }
 
     public void TakeDamage(int amount)
@@ -17,6 +20,7 @@ public class PlayerHealth : MonoBehaviour, IDamageable
 
         currentHP -= amount;
         currentHP = Mathf.Max(0, currentHP);
+        hpText.text = currentHP.ToString();
 
         Debug.Log($"Player took {amount} damage. HP: {currentHP}/{maxHP}");
 
